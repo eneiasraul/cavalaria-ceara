bab1e2fd8275b7a75909c70fcca2a1bebd625644
Eneias Raul <eneias.raul@academico.ifpb.edu.br>
2023-04-24 19:42:33 -0300

    Atualizando condições e idades

--- script.js
+++ script.js
@@ -8,48 +8,48 @@ function verificarCerimonias() {
 
   switch (currentCeremony) {
       case "Nobre Cavaleiro":
-          if (age >= 16 && mesesPassados > 6) {
+          if (age >= 16 && mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro da Capela.";
           } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
           }
           break;
       case "Cavaleiro da Capela":
-          if (age >= 18 || monthsBetween(mesesPassados, new Date()) >= 6) {
+          if (age >= 18 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro da Cruz de Salém.";
           } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
           }
           break;
       case "Cavaleiro da Cruz de Salém":
-          if (age >= 18 || monthsBetween(mesesPassados, new Date()) >= 12) {
+          if (age >= 18 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro Ex-Templário.";
           } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
           }
           break;
       case "Cavaleiro Ex-Templário":
-          if (age >= 21 || monthsBetween(mesesPassados, new Date()) >= 6) {
+          if (age >= 19 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro da Tríade.";
           } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
           }
           break;
       case "Cavaleiro da Tríade":
-          if (age >= 21 || monthsBetween(mesesPassados, new Date()) >= 6) {
+          if (age >= 19 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro do Ébano.";
           } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
           }
           case "Cavaleiro do Ébano":
-            if (age >= 19 || monthsBetween(mesesPassados, new Date()) >= 6) {
+            if (age >= 20 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro Anon.";
             } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
             }
             break;
           case "Cavaleiro Anon":
-            if (age >= 20 || monthsBetween(mesesPassados, new Date()) >= 6) {
+            if (age >= 20 || mesesPassados >= 6) {
               possibleCeremonies = "Você está apto para participar da cerimônia: Cavaleiro da Cadência.";
             } else {
               possibleCeremonies = "Você ainda não atingiu as condições necessárias para participar de outras cerimônias.";
